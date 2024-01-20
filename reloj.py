5194c1efdbbc61c5bd7f0e1838bc220dfc3528c4
manel <manelocana.dev@gmail.com>
2024-01-20 13:34:52 +0100

    retoques

--- reloj.py
+++ reloj.py
@@ -1,25 +1,23 @@
 import time
 import tkinter as tk
 
-
+# ventana
 root = tk.Tk()
 root.title = "reloj"
-root.geometry("400x200")
+root.geometry("300x100")
+root.resizable(False, False)
 
-
+# parametros ventana
 hora =tk.Label(root, text="hora")
-hora.config(bg="yellow", font=("Arial", 40))
-hora.pack()
-
+hora.config(bg="yellow", font=("Arial", 50))
+hora.pack(pady=10)
 
 def reloj():
- 
+    #sacamos hora con modulo time
     hora.config(text=time.strftime("%H:%M:%S"))
+    # actualiza cada segundo
     hora.after(1000, reloj)
 
-
 reloj()
-
-
 
 root.mainloop()
